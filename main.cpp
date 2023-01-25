2920a7cac3d18b66e24253abcf9aebb13cd6de40
Jan Juracka <janjuracka861@seznam.cz>
2023-01-25 17:15:51 +0100

    ADD: Marker preparation

--- main.cpp
+++ main.cpp
@@ -8,6 +8,22 @@ int main(int argc, char** argv) {
 	}
 
 	i3d::Image3d<i3d::GRAY16> img(argv[1]);
+	i3d::Image3d<i3d::GRAY16> out = img;
 
-	img.SaveImage(argv[2]);
+	i3d::GRAY16 max = img.GetMaxValue();
+
+	i3d::Vector3d<std::size_t> size = img.GetSize();
+
+	// Set inner pixels to max
+	for (std::size_t z = 0; z < size.z; ++z) {
+		// Skip first and last iteration for 3d images
+		if (size.z != 1 && (z == 0 || z == size.z - 1))
+			continue;
+
+		for (std::size_t y = 1; y < size.y - 1; ++y)
+			for (std::size_t x = 1; x < size.x - 1; ++x)
+				out.SetVoxel(x, y, z, max);
+	}
+
+	out.SaveImage(argv[2]);
 }
